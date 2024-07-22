fbdf932b05cb648807438c5139c7e2367f58537b
WillStephensonxyz <willstephenson66@gmail.com>
2024-07-22 21:08:14 +1000

    implemented functions and added config file

--- cleanup.py
+++ cleanup.py
@@ -1,50 +1,48 @@
 import os 
-import logging 
-import time 
-from watchdog.observers import Observer 
-from watchdog.events import LoggingEventHandler 
-
-# File Types 
-
-file_extensions = {
-    'audio': [".mp3", ".wav"],
-    'images': [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg"],
-    'document_types': [".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx", ".txt"],
-    'virtual_machines': [".iso", ".img", ".vdi", ".vmdk", ".vhd", ".vdi"]
-}
+import shutil 
+import json 
 
 # Directory Paths 
 
 user = os.path.expanduser("~") 
-
 downloads_path = f"{user}/Downloads" 
+existingFiles = os.scandir(downloads_path) 
 
 audio_path = f"{user}/Music" 
 images_path = f"{user}/Pictures"
 documents_path = f"{user}/Documents" 
 virtual_machine_path = f"{user}/virtualmachines" 
 
-# Create Directories if They Don't Exist 
-
 directory_list = [downloads_path, audio_path, images_path, documents_path, virtual_machine_path] 
 
-print("[+] Checking if Destination Directories Exist") 
+# Check if destination directories exist, create them if not. Then move files 
 
-for dir in directory_list:
-    if os.path.exists(dir) == True: 
-        print(f"{dir} exists") 
-    else: 
-        os.mkdir(dir) 
-        print(f"Created directory at {dir}")
+def moveFiles(file, destination): 
+    print("[+] Checking if Destination Directories Exist") 
+    try:
+        for dir in directory_list:
+            if os.path.exists(dir) == True: 
+                print(f"{dir} exists") 
+            else: 
+                os.mkdir(dir) 
+                print(f"Created directory at {dir}")
+        shutil.move(file, destination)
+    except shutil.Error as e:
+        print(e)
 
-# Scan Downloads Directory and Return List 
+# Map files to extensions and appropriate directory. 
 
-print("[+] Files Currently in Downloads") 
+def sortDownloads(downloads_path):
+    with open("fileconfig.json", "r" ) as f: 
+        file_types = json.load(f) 
 
-for file in os.scandir(downloads_path): 
-    print(file.name) 
-
-print("[+] Moving Files to Corresponding Directories")
+    for file in file_types:
+        destination = file["name"]
 
 
+if __name__ == "__main__": 
+    user = os.path.expanduser("~") 
+    downloads_path = f"{user}/Downloads" 
+    existingFiles = os.scandir(downloads_path) 
 
+    sortDownloads(downloads_path) 
